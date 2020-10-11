b5375887d4fd6c16e185976f6d680c5e580b6d30
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-10-11 16:01:58 -0300

    Ajustando rotina do ponto 3 do trabalho

--- trabalho_n1.sql
+++ trabalho_n1.sql
@@ -23,9 +23,38 @@ end;
 
 $$ language plpgsql;
 
+CREATE OR REPLACE FUNCTION payments_manager_trigger() returns trigger as $$
+
+declare
+    orders_v orders%ROWTYPE;
+    preco_orderdetails integer;
+    quantidade_orderdetails integer;
+	cursor1 refcursor;
+begin
+    
+    open cursor1 for SELECT orders.customernumber, orders.status, orders.ordernumber FROM customers JOIN orders ON customers.customernumber = orders.customernumber WHERE customers.customernumber = NEW.customernumber AND UPPER(orders.status) NOT LIKE 'SHIPPED' AND UPPER(orders.status) NOT LIKE 'CANCELLED';
+	fetch cursor1 into orders_v.customernumber, orders_v.status, orders_v.ordernumber;
+	IF NOT FOUND THEN
+		raise exception 'Não foi encontrado nenhum pedido do customernumber de % com status ON HOLD', NEW.customernumber;		
+	END IF;	
+    SELECT priceeach, quantityordered INTO preco_orderdetails, quantidade_orderdetails FROM orderdetails WHERE ordernumber = orders_v.ordernumber;
+	    
+	IF NEW.amount >= (preco_orderdetails * quantidade_orderdetails) THEN
+        UPDATE orders SET status = 'SHIPPED' WHERE ordernumber = orders_v.ordernumber;
+    END IF;    
+    UPDATE orders SET shippeddate = NEW.paymentdate WHERE ordernumber = orders_v.ordernumber;
+    return NEW;
+end;
+
+$$ language plpgsql;
 
 
 CREATE TRIGGER trigger_estoque 
 	BEFORE INSERT on orderdetails
 	FOR EACH ROW
 	EXECUTE FUNCTION manager_trigger_estoque();
+
+CREATE TRIGGER trigger_pagamento 
+    AFTER INSERT ON payments
+    FOR EACH ROW
+    EXECUTE FUNCTION payments_manager_trigger();
